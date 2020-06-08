dd7301ab7a479c30f5b1c4b0e96444f6b6e555d7
jasbel <asbel.elmec@gmail.com>
2020-06-08 03:22:06 -0400

    @update componentes build

---
+++ build/precache-manifest.63cd875a1982f8f3480aca41375e1b1c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3479541863c69ce1650152c0f46a241d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ac596e0b33284aab3485",
+    "url": "/static/css/main.cc271c11.chunk.css"
+  },
+  {
+    "revision": "cf5f84d1a40b58efbf47",
+    "url": "/static/js/2.e3fb920d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.e3fb920d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ac596e0b33284aab3485",
+    "url": "/static/js/main.c7344390.chunk.js"
+  },
+  {
+    "revision": "822fa42a99ebe8496ebd",
+    "url": "/static/js/runtime-main.1fd70f4a.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
